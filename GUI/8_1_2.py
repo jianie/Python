6e558a368ea87ef158566a0e26b658501fd4cfa8
jianie <77231186+jianie@users.noreply.github.com>
2021-01-13 03:27:26 +0900

    Add files via upload

---
+++ GUI/8_1_2.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+def changeColor(event):          
+    if entName["fg"] == "blue": 
+        entName["fg"] = "red"
+    else:
+        entName["fg"] = "blue"
+        
+window = Tk()     
+window.title("Entry widget")
+entName = Entry(window, fg="blue")
+entName.grid(padx=100, pady=15)
+entName.bind("<Button-3>", changeColor)
+window.mainloop()
+
+
+
+
